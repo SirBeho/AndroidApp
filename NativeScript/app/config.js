82ece17269f18c5adb4e9484418d2c08b79b8391
Sirbeho <Benjamin.tavarez.98@gmail.com>
2024-10-13 23:50:07 -0400

    AWS pipeline

--- NativeScript/app/config.js
+++ NativeScript/app/config.js
@@ -7,7 +7,7 @@ export default {
 
     /*dispositivo*/
     WebSocketUrl: "ws://10.0.2.2:8888/updates",
-    VertxUrl: "http://10.0.2.2:8888",
-    QuarkusUrl : "http://10.0.2.2:9999"
+    VertxUrl: "http://52.90.231.160:8888",
+    QuarkusUrl : "http://44.203.7.224:9999"
 
 };
